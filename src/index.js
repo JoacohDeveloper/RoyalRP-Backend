7ed2daf4abf3bba11ef835296bc8fbd8e1d7ddb1
JoacohDeveloper <joaquinalvarezoficial@outlook.com>
2023-12-17 20:05:48 -0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from "express"
 import env from "dotenv"
+import morgan from "morgan"
 
-
-env.config()
 const app = express()
+app.use(morgan("combined"))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}`)
+})
 
 
